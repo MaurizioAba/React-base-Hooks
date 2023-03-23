574b214dc096c42ade0b6f225cd5cb4af870720b
Mau <123895038+MaurizioAba@users.noreply.github.com>
2023-03-23 22:57:28 +0100

    Add files via upload
    First part hooks, useState

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Inizio from "./lezioni/16-suspense/inizio";
+import {BrowserRouter as Router, Link} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <section className="container text-center my-3">
+      <Router>
+      <nav className="d-flex flex-row justify-content-center mx-auto gap-2 mb-4">
+        <Link to='/'>
+        Home
+        </Link>
+        <Link to='/about'>
+        About
+        </Link>
+        <Link to='/profile'>
+        Profile
+        </Link>
+      </nav>
+      <Inizio />
+      </Router>
+      </section>
+    </div>
+  );
+}
+
+export default App;
